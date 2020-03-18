37dfcbe07bb0642c59184c125e2b85834ae7d163
Mike Conigliaro <mike@conigliaro.org>
2020-03-17 20:21:05 -0600

    Improve exit code handling

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,16 +1,29 @@
 import pytest as pt
 import os
 import updatemyip as umip
+import updatemyip.plugin as plugin
 
 
 @pt.mark.parametrize(
     "args, exit_code",
     [
-        [["noop.example.com", "--no-backoff"], 0],
-        [["example.com", "--no-backoff"], 0],
-        [["fail.example.com", "--no-backoff"], 1],
-        [["example.com", "--no-backoff"], 0],
-    ],
+        [
+            ["noop.example.com", "--no-backoff"],
+            plugin.PLUGIN_STATUS_NOOP
+        ],
+        [
+            ["dry-run.example.com", "--no-backoff", "--dry-run"],
+            plugin.PLUGIN_STATUS_DRY_RUN
+        ],
+        [
+            ["fail.example.com", "--no-backoff"],
+            plugin.PLUGIN_STATUS_FAILURE
+        ],
+        [
+            ["example.com", "--no-backoff"],
+            plugin.PLUGIN_STATUS_SUCCESS
+        ]
+    ]
 )
 def test_main(args, exit_code):
     test_module_paths = [os.path.join(os.path.dirname(__file__), "plugins")]
--- updatemyip/__init__.py
+++ updatemyip/__init__.py
@@ -41,16 +41,13 @@ def main(plugin_module_paths=[], args=None):
     desired_record = f"{opts.fqdn} {opts.dns_ttl} {opts.dns_rrtype} {address}"
     if dns_result == plugin.PLUGIN_STATUS_NOOP:
         log.info(f"No DNS update required")
-        return 0
     elif dns_result == plugin.PLUGIN_STATUS_DRY_RUN:
         log.info(f"DNS will be updated: {desired_record}")
-        return 0
     elif dns_result == plugin.PLUGIN_STATUS_SUCCESS:
         log.info(f"DNS updated: {desired_record}")
-        return 0
     elif dns_result == plugin.PLUGIN_STATUS_FAILURE:
         log.error(f"DNS update failed")
-        return 1
     else:
         log.error(f"DNS plugin returned unknown status: {dns_result}")
-        return 1
+
+    return dns_result
